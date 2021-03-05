05121c0d1400ee6b5dc6138df76aa985ceb69ae7
zongzheng.song@ipanda.co.jp <zongzheng.song@ipanda.co.jp>
2021-03-05 10:23:03 +0900

    Bug修正。

--- src/com/lornwolf/oracle/tool/RemoveModule.java
+++ src/com/lornwolf/oracle/tool/RemoveModule.java
@@ -12,12 +12,12 @@ public class RemoveModule {
         Connection conn = null;
 
         String driver = "oracle.jdbc.driver.OracleDriver";
-        String url = "jdbc:oracle:thin:@47.93.37.155:1521/IAP";
-        String username = "IAP_DEMO";
-        String password = "890-uiop";
-        // String url = "jdbc:oracle:thin:@192.168.244.211:1521/KMCTEST2";
-        // String username = "KMC_11001";
-        // String password = "PWD11001";
+        // String url = "jdbc:oracle:thin:@47.93.37.155:1521/IAP";
+        // String username = "IAP_DEMO";
+        // String password = "890-uiop";
+        String url = "jdbc:oracle:thin:@192.168.244.211:1521/KMCTEST2";
+        String username = "KMC_11001";
+        String password = "PWD11001";
 
         try {
             Class.forName(driver);
@@ -34,9 +34,10 @@ public class RemoveModule {
                 String tableType = tables.getString("TABLE_TYPE");
                 String tableName = tables.getString("TABLE_NAME");
 
+                // 出力删除表或数据的SQL。
                 if (tableType.toUpperCase().equals("TABLE")) {
                     if (tableName.toUpperCase().contains("P17")) {
-                        System.out.println(tableName);
+                        System.out.println("TABLE : " + tableName);
                         dropTableStream.write(("DROP TABLE " + username + "." + tableName + ";\r\n").getBytes());
                     } else {
                         ResultSet resultSet = stmt.executeQuery("select t.column_name from user_col_comments t where t.table_name = '" + tableName + "'");
@@ -51,10 +52,11 @@ public class RemoveModule {
                     }
                 }
 
+                // 出力删除视图的SQL。
                 if (tableType.toUpperCase().equals("VIEW")) {
                     if (tableName.toUpperCase().contains("P17")) {
                         System.out.println("VIEW : " + tableName);
-                        dropTableStream.write(("DROP VIEW " + username + "." + tableName + ";\r\n").getBytes());
+                        dropViewStream.write(("DROP VIEW " + username + "." + tableName + ";\r\n").getBytes());
                     }
                 }
             }
@@ -64,6 +66,7 @@ public class RemoveModule {
             dropViewStream.flush();
             dropViewStream.close();
 
+            // 出力删除代码的SQL。
             FileOutputStream dropSourceStream = new FileOutputStream("D:/drop_source.sql");
             ResultSet resultSet = stmt.executeQuery("SELECT DISTINCT OWNER, NAME, TYPE FROM ALL_SOURCE WHERE OWNER = '" + username + "'");
             while(resultSet.next()) {
